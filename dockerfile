0714078bf791030f6b7e8286d0ca3c4d0eb03a6e
Abijith S <abijithsuresh47@gmail.com>
2025-04-08 15:29:57 +0530

    Updated eureka-server to use gradle

--- dockerfile
+++
@@ -1,3 +0,0 @@
-FROM openjdk:21-oracle
-COPY ./target/Eurekaserver-0.0.1-SNAPSHOT.jar registry-service.jar
-CMD ["java","-jar","registry-service.jar"]
--- src/test/java/sh/abijith/eureka_server/EurekaServerApplicationTests.java
+++ src/test/java/sh/abijith/eureka_server/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sharathinfotech.Eurekaserver;
+package sh.abijith.eureka_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaserverApplicationTests {
+class EurekaServerApplicationTests {
 
 	@Test
 	void contextLoads() {
